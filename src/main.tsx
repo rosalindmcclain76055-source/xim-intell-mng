21e169d4a73c947ddc6fabc4a70c9ba8875e5c69
rosalindmcclain76055-source <rosalindmcclain76055@gmail.com>
2026-04-25 06:44:08 -0400

    Update src/main.tsx with new content on 2026-04-25 10:43:49 UTC

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,9 @@ import App from "./App.tsx";
 import "./index.css";
 import "./i18n";
 
+// Handle SPA routing redirect from 404.html
+if (sessionStorage.redirect) {
+  delete sessionStorage.redirect;
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
